94e235091f95c47fb7455e1a20cfae696e6e31f9
ilham nayoko <nayokoilham@gmail.com>
2023-09-11 14:02:43 +0700

    make controller and view

--- app/Http/Controllers/Harga/HargaPasangController.php
+++ app/Http/Controllers/Harga/HargaPasangController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Harga;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class HargaPasang extends Controller
+class HargaPasangController extends Controller
 {
     /**
      * Display a listing of the resource.
--- resources/views/harga/index.blade.php
+++ resources/views/harga/index.blade.php
@@ -5,12 +5,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite('resources/css/app.css')
+    <title>Harga MDU</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
-
+    <h1 class="text-3xl font-bold">Test</h1>
 </body>
 
 </html>
